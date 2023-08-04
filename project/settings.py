5a09328b5a88761d8775765c8ce5710ec05199d4
Kamclose123 <141050642+Kamclose123@users.noreply.github.com>
2023-08-04 23:53:36 +0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8^(e3@n4m+*1^fga-^pf@umn18&1(xj@yj9k-l-k%q7_c@oby@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'Trader/static'),
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Trader/static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'Trader/static')
 
 
